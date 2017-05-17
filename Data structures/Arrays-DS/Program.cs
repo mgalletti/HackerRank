c0ea7226e1213e91cd84528aa4becaaa4b3c5f94
mgalletti <superga8611@gmail.com>
2017-05-17 18:11:57 +0200

    Update Program.cs
    solution to task added

--- Data structures/Arrays-DS/Program.cs
+++ Data structures/Arrays-DS/Program.cs
@@ -8,5 +8,6 @@ class Solution {
         int n = Convert.ToInt32(Console.ReadLine());
         string[] arr_temp = Console.ReadLine().Split(' ');
         int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        Console.WriteLine(string.Join(" ", arr.Reverse()));
     }
 }
